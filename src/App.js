fb18c797a6c103835b945819c64ff198df769d88
Kevin Fergus <kevinfergus89@gmail.com>
2020-08-10 11:02:07 -0500

    range position is dynamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Slider, { Range } from 'rc-slider';
@@ -6,10 +6,17 @@ import 'rc-slider/assets/index.css';
 import returns from './returns';
 function App() {
 	const [ range, setRange ] = useState([ 1926, 2020 ]);
+	const [ rangePositon, setRangePosition ] = useState(window.innerWidth / 4);
 
 	function changeRange(e) {
 		setRange(e);
 	}
+	useEffect(() => {
+		function handleResize() {
+			setRangePosition(window.innerWidth / 4);
+		}
+		window.addEventListener('resize', handleResize);
+	});
 
 	const returnsRange = returns.slice(range[0] - 1926, range[1] - 1926 + 1);
 	returnsRange.map(function(entry, i) {
@@ -22,14 +29,16 @@ function App() {
 	return (
 		<div className="App">
 			<Range
+				style={{ maxWidth: window.innerWidth / 2, left: rangePositon }}
 				min={1926}
 				max={2020}
 				defaultValue={[ 1926, 2020 ]}
 				allowCross={false}
 				onChange={(e) => changeRange(e)}
 			/>
-			S&P 500 Total Returns by Year
-			<table className="table">
+			<div style={{ fontSize: 50 }}>S&P 500 Total Returns by Year</div>
+
+			<table className="table" style={{ fontSize: 20 }}>
 				<div>
 					<tr>
 						<td>Year</td> <td>Total Return</td> <td>Cumulative</td>
