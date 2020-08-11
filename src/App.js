5ae160bba9aed6bb9f6b6c111fdfbbeb54485a61
Kevin Fergus <kevinfergus89@gmail.com>
2020-08-11 10:52:46 -0500

    slider has values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Slider, { Range } from 'rc-slider';
+import Slider from 'rc-slider';
+
 import 'rc-slider/assets/index.css';
 import returns from './returns';
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
 function App() {
-	const [ range, setRange ] = useState([ 1926, 2020 ]);
+	const [ range, setRange ] = useState([ 1926, 2019 ]);
 	const [ rangePositon, setRangePosition ] = useState(window.innerWidth / 4);
+	const marks = {
+		'1926': '2019',
+		1926: <strong>1926</strong>,
+		2019: <strong>2019</strong>
+	};
 
 	function changeRange(e) {
 		setRange(e);
@@ -31,7 +41,8 @@ function App() {
 			<Range
 				style={{ maxWidth: window.innerWidth / 2, left: rangePositon }}
 				min={1926}
-				max={2020}
+				max={2019}
+				marks={marks}
 				defaultValue={[ 1926, 2020 ]}
 				allowCross={false}
 				onChange={(e) => changeRange(e)}
